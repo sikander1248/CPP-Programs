35b586c59c247fa18191d134fc0a42695e4fe7b0
MOHAMMED SIKANDER <sikander1248@gmail.com>
2016-08-24 17:48:46 +0530

    added difference in days

--- date.cpp
+++ date.cpp
@@ -13,10 +13,20 @@ public :Date(int day,int month,int year) {
         void print()  {
             cout <<day<<"-"<<month<<"-"<<year<<endl;
         }
+        int differenceInDays(const Date &endDate){
+            //Assuming every month has 30 days.
+            int days = (endDate.month - this->month) * 30;
+                days = days + (endDate.day - this->day);
+            return days;
+        }
 };
 int main()
 {
-    Date    birthDate(25,8,1973);
+    Date    issueDate(25 , 8  , 2016);
+    Date    returnDate  (2 , 10 , 2016);
+    cout <<"Book was borrowed for "
+         << issueDate.differenceInDays(returnDate)
+         <<" days"<<endl;
 
-    birthDate.print();
+    return 0;
 }
