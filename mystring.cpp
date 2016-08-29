339a7b790d62c1fca78aed8e401aae1af3895d9d
MOHAMMED SIKANDER <sikander1248@gmail.com>
2016-08-29 17:39:42 +0530

    Added Copy Constructor

--- mystring.cpp
+++ mystring.cpp
@@ -11,26 +11,31 @@ class MyString
          str = NULL;
     }
 	MyString(const char *ptr)
-	{
+	{   cout <<"Parameterized Constructor " << endl;
 		int len = strlen(ptr);
 		str = new char[len + 1];
 		strcpy(str , ptr);
     }
     void display(){
-        cout << str << endl;
+        cout <<(int *)str <<" "<<str << endl;
     }
     ~MyString(){
+        cout <<"Destructor " << endl;
         delete [] str;
+    }
+    MyString(const MyString &old){
+        cout <<"Copy Constructor "<<endl;
+        int length = strlen(old.str);
+        str = new char[length + 1];
+        strcpy(str , old.str);
     }
 };
 
  int main( )
  {
 	MyString	s1 = "SIKANDER";
+	MyString    s2 = s1;
 	s1.display();
+	s2.display();
 	return 0;
  }
-
-
-
-
