65d734b93099613539417a7c2000bb3ea1d8eacf
MOHAMMED SIKANDER <sikander1248@gmail.com>
2016-09-08 15:15:26 +0530

    added queue and inheritance

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-class MyStack
+
+class LinearDS
+{
+public :
+	 virtual void push(int ele) = 0;
+	 virtual int pop()= 0;
+	 virtual int  getNext() const = 0;
+	 virtual bool empty( ) const = 0;
+};
+
+class MyStack : public LinearDS
 {
 	const int SIZE;
 	int *buffer;
@@ -8,9 +18,53 @@ class MyStack
 public : MyStack(int p_size);
 	 void push(int ele);
 	 int pop();
-	 int  getTop() const;
+	 int  getNext() const;
 	 bool empty( ) const;
 };
+
+class MyQueue : public LinearDS
+{
+	const int SIZE;
+	int *buffer;
+	int rearIndex;
+	int frontIndex;
+public : MyQueue(int p_size);
+	 void push(int ele);
+	 int pop();
+	 int  getNext() const;
+	 bool empty( ) const;
+};
+MyQueue::MyQueue(int p_size) : SIZE(p_size)
+{
+    buffer = new int[SIZE];
+	rearIndex = -1;
+	frontIndex = 0;
+}
+
+void MyQueue::push(int ele)
+{
+    if(rearIndex < SIZE - 1)
+        buffer[++rearIndex] = ele;
+    else
+        cout << "Queue Full" << endl;
+}
+
+int MyQueue::pop()
+{
+    if(frontIndex > rearIndex)
+        return 0;
+    return buffer[frontIndex++];
+}
+int MyQueue::getNext() const
+{
+    return buffer[frontIndex];
+}
+
+bool MyQueue::empty() const
+{
+    return frontIndex > rearIndex ? true : false;
+}
+
 
 MyStack::MyStack(int p_size) : SIZE(p_size)
 {
@@ -32,7 +86,7 @@ int MyStack::pop()
         return 0;
     return buffer[topIndex--];
 }
-int MyStack::getTop() const
+int MyStack::getNext() const
 {
     if(topIndex == -1)
         return 0;
@@ -47,38 +101,38 @@ bool MyStack::empty() const
 int main()
 {
     /* Code to Test Stack */
-    enum StackOperations{PUSH = 1, POP , GETTOP , EMPTY};
+    enum {PUSH = 1, POP , GETNEXT , EMPTY};
 
-    int stacksize;
-    cout <<"Enter the size of Stack : ";
-    cin >> stacksize;
-    MyStack    s1 = MyStack(stacksize);
+    int size,dschoice;
+    LinearDS *pds;
+    cout <<"Enter 1 for Stack 2. Queue \n";
+    cin >> dschoice;
+    cout <<"Enter the size of Data Structure : ";
+    cin >> size;
+
+    if(dschoice == 1)
+        pds = new MyStack(size);
+    else
+        pds = new MyQueue(size);
+
 
     int ch , ele;
     for(;;)
     {
-        cout <<"\n 1. PUSH \n 2. POP \n 3. GETTOP \n 4. EMPTY \n";
+        cout <<"\n 1. PUSH \n 2. POP \n 3. GETNEXT \n 4. EMPTY \n";
         cin >> ch;
         switch(ch)
         {
         case PUSH : cout << "Enter the element to insert ";
                     cin >> ele;
-                    s1.push(ele);
+                    pds->push(ele);
                     break;
-        case POP : cout << s1.pop();
+        case POP : cout << pds->pop();
                    break;
-        case GETTOP : cout << s1.getTop(); break;
-        case EMPTY : cout << s1.empty(); break;
+        case GETNEXT : cout << pds->getNext(); break;
+        case EMPTY : cout << pds->empty(); break;
         default : return 0;
         }
     }
     return 0;
 }
-
-
-
-
-
-
-
-
