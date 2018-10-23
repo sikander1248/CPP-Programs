bd49110c96d145031fc287132951f268a1c6e706
MOHAMMED SIKANDER <sikander1248@gmail.com>
2018-10-23 11:43:05 +0530

    Pass by reference

--- thread_argument_passing.cpp
+++ thread_argument_passing.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <thread>
 using namespace std;
-void fun1(int x)
+void fun1(int &x)
 {
     cout << "Argument recieved" << x << endl;
     x = x * 2;
     cout << "Inside thread Updated x = "<<x << endl;
 }
 
-
 int main()
 {
     int a = 5;
-
-    thread t1(fun1 , a);
+    //thread t1(fun1 , a);  // compilation error as the argument is recieved by reference.
+    thread t1(fun1 , ref(a));  
 
     t1.join();
 
     cout << "Main Thread a = " <<a << endl;
     return 0;
 }
-
