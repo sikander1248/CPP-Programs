b48c51a7897524c4518f9f8af4e750507a166deb
MOHAMMED SIKANDER <sikander1248@gmail.com>
2019-08-22 17:56:01 +0530

    Updated to Ranged Based for loop

--- range_based_for.cpp
+++ range_based_for.cpp
@@ -9,9 +9,9 @@ int main()
 {
 	int a[] = {12,23,34,56,78};
 	
-	int n = sizeof(a) / sizeof(int);
-	for(int i = 0 ; i < n ; i++)
-		cout << a[i] <<" ";
+	for(int ele : v)
+		cout <<ele <<" ";
+	cout << endl;
 	
 	vector<int> v;
 	v.push_back(12);
@@ -19,20 +19,17 @@ int main()
 	v.push_back(34);
 	v.push_back(45);
 
-	
-	for(int i = 0 ; i < v.size() ; i++)
-		cout << v[i] <<" ";
+	for(int ele : l)
+		cout <<ele <<" ";
+	cout << endl;
 	
 	list<int> l;
 	l.push_back(90);
 	l.push_back(78);
 	
-	list<int>::iterator it = l.begin();
-	while(it != l.end())
-	{
-		cout << *it <<" ";
-		++it;
-	}
+	for(int ele : l)
+		cout <<ele <<" ";
+	cout << endl;
 	
 	
 }
