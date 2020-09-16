1cd21c961ea725a2afad47f2145d50e0bfe200cf
MOHAMMED SIKANDER <sikander1248@gmail.com>
2020-09-16 19:08:13 +0530

    Add files via upload

---
+++ ModernC++/STL_Containers/forward_list_delete_key.cpp
@@ -0,0 +1,54 @@
+/*
+    Program to search for a given key element and delete it.
+    Forward List does not provide delete at given address, it provides delete after.
+    To delete a node with given address, we need to copy the next node's data to current node
+    and delete the next node.
+*/
+#include <iostream>
+#include <algorithm>
+#include <forward_list>
+
+using namespace std;
+
+void display(forward_list<int> fl)
+{
+    for(int ele : fl)
+        cout << ele <<" ";
+    cout << endl;
+}
+
+ int main()
+ {
+    forward_list<int>  fl = {12,23,34,45,56};
+
+    int key;
+    cout <<"Enter  the element to delete : ";
+    cin >> key;
+
+    forward_list<int>::iterator it = find(begin(fl), end(fl), key);
+
+    if(it == end(fl))
+    {
+        cout <<"Element not found \n";
+    }
+    else
+    {
+        cout <<"Element found \n";
+        forward_list<int>::iterator nextIt = it;
+        nextIt++;
+        *it = *nextIt;
+        fl.erase_after(it);
+        cout <<"Forward List elements after deletion : ";
+        display(fl);
+    }
+ }
+
+
+
+
+
+
+
+
+
+
