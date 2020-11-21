419a461bd6b67fbc8b52dd79c10db11e6d4d5677
MOHAMMED SIKANDER <sikander1248@gmail.com>
2020-11-21 11:07:30 +0530

    Shallow Copy

---
+++ Shallow_Deep_Copy.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+class MyString
+{
+    char *buffer = nullptr;
+public :
+    MyString() = default;
+
+    MyString(const char *str)
+    {
+        int len = strlen(str);
+        buffer = new char[len + 1];
+        strcpy(buffer, str);
+    }
+    MyString(const MyString &rhs) = default;
+
+    void modify(const char *newData)
+    {
+        strcpy(buffer, newData);
+    }
+    void display()
+    {
+        cout << buffer << endl;
+    }
+};
+//Default Functions and  Deleted Function
+
+int main( )
+{
+    MyString  ms1 ("Sikander"); // Parameterized Constructor
+    MyString  ms2 = ms1;        // Shallow Copy
+
+    cout <<"Contents of ms1 : ";
+    ms1.display();
+    cout <<"Contents of ms2 : ";
+    ms2.display();
+
+    ms2.modify("Keerthi");
+    cout <<"-----------------------\n";
+    cout <<"Contents of ms1 : ";
+    ms1.display();                      //Keerthi
+    cout <<"Contents of ms2 : ";
+    ms2.display();
+
+}
