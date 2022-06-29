60c11a7ee762fdbe9805129a34b49d4f9be05474
MOHAMMED SIKANDER <sikander1248@gmail.com>
2022-06-29 15:06:19 +0530

    updated the shared resource
    running the threads parallel and updating the shared resource

--- ModernC++/multithreading/race_condition.cpp
+++ ModernC++/multithreading/race_condition.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <thread>
 using namespace std;
@@ -23,14 +15,14 @@ void square(int x) {
 
 int main() {
 	thread t1(square , 2);
-	t1.join();
 
 	thread  t2(square , 3);
-	t2.join();
 
 	thread  t3(square , 4);
+	t1.join();
+	t2.join();
 	t3.join();
     	
 	cout << "Final Sum = " << sum << endl;
-  return 0;
+    	return 0;
 }
