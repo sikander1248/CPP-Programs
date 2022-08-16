b3fa95bfa1d830d0d4e1de00385a7f4aa0e69122
MOHAMMED SIKANDER <sikander1248@gmail.com>
2022-08-16 17:17:03 +0530

    Added user defined literals

--- ModernC++/user_defined_literals.cpp
+++ ModernC++/user_defined_literals.cpp
@@ -1,11 +1,30 @@
-#include <iostream>
-
+#include <iostream>										
 using namespace std;
 
+long double operator "" _mm(long double x)
+{
+	return x;
+}
+long double operator "" _cm(long double x)
+{
+	return x * 10;
+}
+long double operator "" _m(long double x)
+{
+	return x * 1000;
+}
 int main()
 {
-	long double height = 5.0;
+	long double length = 4.0_mm;
+	long double width = 2.0_m;
+	long double height = 5.0_cm;
 
+	cout << "Length : " << length << endl;
+	cout << "Width  : " << width << endl;
+	cout << "Height : " << height << endl;
+
+	long double volume = length * width * height;
+	cout << "Volume : " << volume << endl;
 	return 0;
 }
 
